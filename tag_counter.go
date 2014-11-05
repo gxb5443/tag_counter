371f880d1ff5be8cc26b25c648ea6b2404b1aaf2
Gian Biondi <gianbiondijr@gmail.com>
2014-11-05 14:20:29 -0500

    Sets up basic server skeleton

--- tag_counter.go
+++ tag_counter.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Tag struct {
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Web Server Online! Param is %s!", r.URL.Path[1:])
+}
+
+func taghandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "You got it alright!")
+	} else {
+		fmt.Fprintf(w, "Sorry, %s functionality not yet supported", r.Method)
+	}
+}
+
+func tag2handler(w http.ResponseWriter, r *http.Request) {
+	req_tag := r.URL.Path[len("/tag/"):]
+	if r.Method == "GET" {
+		fmt.Fprintf(w, "Looking for tag: \"%s\"", req_tag)
+	} else {
+		fmt.Fprintf(w, "Sorry, %s functionality not yet supported", r.Method)
+	}
+}
+
+func main() {
+	fmt.Printf("Starting Web Server...")
+	http.HandleFunc("/tag/", tag2handler)
+	http.HandleFunc("/tag", taghandler)
+	http.HandleFunc("/", handler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Could not start server: %s", err)
+	}
+}
